385365def2e8c44a46654df46629b395960f6d64
sergiocc <2sergiocuadrado2000@gmail.com>
2024-06-19 10:48:47 +0200

    maven deploy

--- ms_contactos/src/main/java/Controller/ContactosController.java
+++ ms_contactos/src/main/java/Controller/ContactosController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import model.Contacto;
 import service.AgendaService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ContactosController {
 	
--- ms_contactos/src/test/java/com/example/demo/MsContactosApplicationTests.java
+++ ms_contactos/src/test/java/com/example/demo/MsContactosApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import main.MsContactosApplication;
+
+@SpringBootTest(classes = MsContactosApplication.class)
 class MsContactosApplicationTests {
 
 	@Test
